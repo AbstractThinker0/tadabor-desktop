b4dcd2b93dcc2ca3ae6f3f51991b405f2593188a
EnlightenCode <arabicwriter09@gmail.com>
2023-05-17 09:23:44 +0100

    Refactor and optimize notes handling:
    * Utilize Redux Toolkit for efficient management of notes state
    * Optimize notes loading to occur only once during app initialization
    * Refactor NotesText component to eliminate redundancy

--- src/react/index.tsx
+++ src/react/index.tsx
@@ -2,25 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { HashRouter } from "react-router-dom";
+
+import store from "./store";
+import { Provider } from "react-redux";
+
 import Layout from "./components/Layout/Layout";
 import App from "./App";
 
 import "./i18n";
+import DataLoader from "./components/DataLoader";
 
-function render() {
-  const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
-  );
-
-  root.render(
-    <React.StrictMode>
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
       <HashRouter>
         <Layout>
-          <App />
+          <DataLoader>
+            <App />
+          </DataLoader>
         </Layout>
       </HashRouter>
-    </React.StrictMode>
-  );
-}
-
-render();
+    </Provider>
+  </React.StrictMode>
+);
