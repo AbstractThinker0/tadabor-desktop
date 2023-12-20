f5ec056459f9613a1613a616fa1bf03ea9202417
AbstractThinker0 <arabicwriter09@gmail.com>
2023-12-20 18:24:43 +0100

    QuranBrowser: Fix search word not picking the currently selected chapter correctly

--- src/react/reducers/qbActionSearchWord.ts
+++ src/react/reducers/qbActionSearchWord.ts
@@ -7,14 +7,15 @@ export function qbSearchWord(
   chapterNames: chapterProps[],
   allQuranText: quranProps[]
 ): qbStateProps {
+  // Destruct current state
   const {
     searchString,
     searchMethod,
-    selectChapter,
     selectedChapters,
     searchDiacritics,
     searchIdentical,
   } = state;
+
   // initial search state
   const newState: qbStateProps = {
     ...state,
@@ -29,66 +30,37 @@ export function qbSearchWord(
     scrollKey: "",
   };
 
+  // If an empty search token don't initiate a search
   if (onlySpaces(searchString)) {
     return { ...newState, searchError: true };
   }
 
+  // Check if we are search with diacrtics or they should be stripped off
   const normalizedToken = searchDiacritics
     ? searchString
     : removeDiacritics(searchString);
 
+  // If the token only had diacrtics this will return true
   if (onlySpaces(normalizedToken)) {
     return { ...newState, searchError: true };
   }
 
   const matchVerses: verseMatchResult[] = [];
 
-  if (selectedChapters.length === 114) {
-    allQuranText.forEach((sura) => {
-      sura.verses.forEach((verse) => {
-        const result = searchVerse(
-          verse,
-          normalizedToken,
-          searchIdentical,
-          searchDiacritics
-        );
+  selectedChapters.forEach((chapter) => {
+    allQuranText[Number(chapter) - 1].verses.forEach((verse) => {
+      const result = searchVerse(
+        verse,
+        normalizedToken,
+        searchIdentical,
+        searchDiacritics
+      );
 
-        if (result) {
-          matchVerses.push(result);
-        }
-      });
+      if (result) {
+        matchVerses.push(result);
+      }
     });
-  } else {
-    if (selectedChapters.length > 1) {
-      selectedChapters.forEach((chapter) => {
-        allQuranText[Number(chapter) - 1].verses.forEach((verse) => {
-          const result = searchVerse(
-            verse,
-            normalizedToken,
-            searchIdentical,
-            searchDiacritics
-          );
-
-          if (result) {
-            matchVerses.push(result);
-          }
-        });
-      });
-    } else {
-      allQuranText[selectChapter - 1].verses.forEach((verse) => {
-        const result = searchVerse(
-          verse,
-          normalizedToken,
-          searchIdentical,
-          searchDiacritics
-        );
-
-        if (result) {
-          matchVerses.push(result);
-        }
-      });
-    }
-  }
+  });
 
   if (matchVerses.length === 0) {
     return { ...newState, searchError: true };
