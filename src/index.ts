d9e767b5bec198fc0a44bcc284371b5892da378a
AbstractThinker0 <arabicwriter09@gmail.com>
2024-01-13 18:54:08 +0100

    Ref: inline electron-squirrel-startup package

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { app, BrowserWindow, shell } from "electron";
+import path from "path";
+import { spawn } from "child_process";
 
 import contextMenu from "electron-context-menu";
 contextMenu({
@@ -15,8 +17,43 @@ declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require("electron-squirrel-startup")) {
-  app.quit();
+// code extracted from https://www.npmjs.com/package/electron-squirrel-startup
+if (process.platform === "win32") {
+  const updateExe = path.resolve(
+    path.dirname(process.execPath),
+    "..",
+    "Update.exe"
+  );
+
+  const run = function (args: [string], done: () => void) {
+    spawn(updateExe, args, {
+      detached: true,
+    }).on("close", done);
+  };
+
+  const check = function () {
+    const cmd = process.argv[1];
+
+    const target = path.basename(process.execPath);
+
+    if (cmd === "--squirrel-install" || cmd === "--squirrel-updated") {
+      run(["--createShortcut=" + target + ""], app.quit);
+      return true;
+    }
+    if (cmd === "--squirrel-uninstall") {
+      run(["--removeShortcut=" + target + ""], app.quit);
+      return true;
+    }
+    if (cmd === "--squirrel-obsolete") {
+      app.quit();
+      return true;
+    }
+    return false;
+  };
+
+  if (check()) {
+    app.quit();
+  }
 }
 
 const createWindow = (): void => {
